89c18259e9a7c401b77bb46a0ce39655c6b01c4b
Kamal Mukherjee <kamalnyc17@gmail.com>
2019-05-26 17:55:17 -0400

    Changed the header background color to #5133a4

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function NavTabs(props) {
   return(
-    <div className="container">
-      <div className="row text-white bg-info">
+    <div className="container sticky-top">
+      <div className="row text-white" style ={{backgroundColor:'#5133a4'}}>
         <div className="col-12">
           <h1 className="display-4 text-center">Clicky Game</h1>
           <p className="text-center font-weight-bold">Click on an Image to Earn Points, but don't click on the same image twice!</p>
