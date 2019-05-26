e7e9b33341581daabb0a347313d58f4237d13df4
Kamal Mukherjee <kamalnyc17@gmail.com>
2019-05-26 12:07:40 -0400

    Updates

---
+++ precache-manifest.755e68caca42d5fe65efda8af4807e30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f650534089e52f1684b90abe8fb5154d",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "f21f300854c908b359da",
+    "url": "/clickygame/static/css/main.5644e3b4.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clickygame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "f21f300854c908b359da",
+    "url": "/clickygame/static/js/main.248030fc.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
